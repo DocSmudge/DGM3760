0573ab7c65be6f994481846845719c06357a7ff4
Gabrielle Edwards Account <10971828@uvu.edu>
2023-02-12 11:00:44 -0700

    Just figuring out JS for nav

--- unit01/js/script.js
+++ unit01/js/script.js
@@ -6,3 +6,21 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerButton");
 
 x.onclick = toggleMenu;
+
+var test = document.querySelectorAll("ul#primaryNav li a");
+console.log(test);
+
+var truck = "book.html";
+
+var i;
+for (i = 0; i < test.length; i++) {
+  var myPage = test[i].getAttribute("href");
+  console.log(myPage);
+
+  if (truck === myPage) {
+    test[i].parentNode.className = "active";
+    test[i].parentNode.parentNode.parentNode.className = "parent";
+  } else {
+    test[i].parentNode.className = "";
+  }
+} // end of loop
